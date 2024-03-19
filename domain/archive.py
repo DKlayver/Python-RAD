2da913bf8764de6cb9655277be820b9463e6ab96
Davi Klayver <davi.klayveR@live.com>
2024-03-18 21:34:16 -0300

    feature - criando metodo de ler csv, implementando a main e criando transparance.py

--- domain/archive.py
+++ domain/archive.py
@@ -1,5 +1,7 @@
 import csv
 
+from model.transparance import Transparence
+
 
 class Archive:
     def __init__(self, path_file):
@@ -8,3 +10,33 @@ class Archive:
     def create_archive(self):
         with open(self.path_file, "w", encoding="utf-8") as file:
             csv.writer(file, delimiter=";")
+
+    def file_read(self):
+        with open(self.path_file, "r", encoding="utf-8") as file:
+            leitor = csv.reader(file, delimiter=";")
+
+            next(leitor, None)
+            dados = []
+            for linha in leitor:
+                transparencia = Transparence(
+                    linha[0],
+                    linha[1],
+                    linha[2],
+                    linha[3],
+                    linha[4],
+                    linha[5],
+                    linha[6],
+                    linha[7],
+                    linha[8],
+                    linha[9],
+                    linha[10],
+                    linha[11],
+                    linha[12],
+                    linha[13],
+                    linha[14],
+                    linha[15],
+                )
+
+                dados.append(transparencia)
+
+            return dados
